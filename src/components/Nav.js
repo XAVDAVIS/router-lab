64b648498388df5fe7520d6f581cfa935124668e
Xavier <shottyx5156@hotmail.com>
2023-01-10 23:13:35 -0500

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Nav = (props) => {
+    return (
+        <div className="nav">
+            <Link to="/">
+                <div>HOME</div>
+            </Link>
+            <Link to="/about">
+                <div>ABOUT</div>
+            </Link>
+            <Link to="/stocks/:symbol">
+                <div>STOCK</div>
+            </Link>
+            <Link to="/stocks">
+                <div>DASHBOARD</div>
+            </Link>
+        </div>
+    );
+};
+
+export default Nav;
