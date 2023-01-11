4fb658ce2a05121e9585b3c722a0a78af32918bd
Xavier <shottyx5156@hotmail.com>
2023-01-11 07:38:22 -0500

    Added show stocks page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
+import About from './pages/About';
+import Main from './pages/Main';
+import Stocks from './pages/Stocks';
+
 
 function App() {
   return (
     <div className="App">
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Main />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>  
+        <Route path="/showstock/:symbol"
+        render={(routerProps) => <ShowStocks {...routerProps} />}
+      />
+      </Switch>
       
     </div>
   );
---
+++ src/pages/ShowStock.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import stocks from '../stock-data';
+
+const ShowStock = (props) => {
+    const symbol = props.match.params.symbol;
+    
+    console.log(symbol);
+
+    const [stock, setStock] = React.useState(null);
+
+    const getStock = async () => {
+        const response = await fetch(url); 
+        const data = await response.json();
+        setStock(data);
+    };
+
+    React.useEffect(() => {
+        getStock();
+    }, []);
+
+    const loaded = () => {
+        return (
+            <>
+            <div>
+                <div>Stock Show Page</div>
+                <h1>{stock.symbol}</h1>
+                <h1>{stock.name}</h1>
+                <p>Last Price: {stock.lastPrice}</p>
+                <p>Change: {stock.change}</p>
+                <p>High: {stock.high}</p>
+                <p>Low: {stock.low}</p>
+                <p>Open: {stock.open}</p>
+            </div>
+            </>
+        );
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return stock ? loaded() : loading();
+};
+
+export default ShowStock;
