9ac62078f6faadbe36289dcef6ab0bc8c7074eec
Xavier <shottyx5156@hotmail.com>
2023-01-11 00:00:51 -0500

    added stocks data

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = (props) => {
+    return (
+        <>
+        <div className='aboutpage'>
+        <h1>ABOUT</h1>
+        <p>Listing Stocks</p>
+        </div>
+        </>
+    );
+};
+
+export default About;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Main =(props) => {
-    return <h1>This is the Main Component</h1>;
+    return (
+        <div>HOME</div>
+    );
 };
 
 export default Main;
